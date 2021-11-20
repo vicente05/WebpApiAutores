55c9fa18a0f4a9e9ea0c7c6559311d2dcd50e958
Vicente <vicentemline@gmail.com>
2021-11-20 11:39:42 +0100

    prueba del gitlen

--- ApplicationDbcontext.cs
+++ ApplicationDbcontext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using WebpApiAutores.Models;
+using WebpApiAutores.Entidades;
 
 namespace WebpApiAutores
 {
@@ -12,8 +12,18 @@ namespace WebpApiAutores
 
         public ApplicationDbcontext(DbContextOptions options) : base(options) { }
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AutorLibro>().HasKey(al => new { al.autorid, al.libroid });
+        }
+
         public DbSet<Autor> Autores { get; set; }
         public DbSet<Libro> Libros { get; set; }
+        public DbSet<Comentario> Comentarios { get; set; }
+        public DbSet<AutorLibro> AutorLibro { get; set; }
 
     }
 }
